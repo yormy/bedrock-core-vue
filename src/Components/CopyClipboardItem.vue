01b2ccae9b03769b2d96a494caa95f50a689b72e
rob <dev@mexion.com>
2021-09-10 12:11:25 +0200

    copy item to clipboard and ui

---
+++ src/Components/CopyClipboardItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <span>
+    <span v-if="isCopied" class="fal fa-clipboard"></span>
+
+    <span v-else @click="copyClipboard()">
+      <slot></slot>
+    </span>
+
+  </span>
+</template>
+
+<script>
+export default {
+
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      isCopied: false,
+      valueOnClipboard: '',
+      timeoutInMs: 1000,
+    }
+  },
+
+  methods: {
+    copyClipboard() {
+      this.isCopied = true
+
+      const self = this;
+      navigator.clipboard.writeText(this.value).then(function () {
+        self.valueOnClipboard = this.value;
+      }, function (err) {
+        console.error('Could not copy text: ', err);
+      });
+
+      window.setTimeout(() => {
+        this.isCopied = false
+      }, this.timeoutInMs);
+    },
+  }
+}
+</script>
