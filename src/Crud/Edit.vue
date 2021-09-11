af9653e3832be438f9d9e16e7349350a1054a836
rob <dev@mexion.com>
2021-09-11 16:17:58 +0200

    added delete function

--- src/Crud/Edit.vue
+++ src/Crud/Edit.vue
@@ -40,7 +40,8 @@ export default {
         this.RefreshParent();
         window.close();
       }
-      window.location.href = (this.route(this.routes.index));
+      //window.location.href = (this.route(this.routes.index));
+      window.location.href = this.redirectAfterUpdate();
     },
 
     RefreshParent() {
@@ -53,11 +54,16 @@ export default {
       return data;
     },
 
+    // override in child to pass additional parameters
+    addItemRouteParameters() {
+      return this.model.xid;
+    },
+
     getSaveUrl() {
       if (this.model.xid) {
-        return this.route(this.routes.update, this.model.xid);
+        return this.route(this.routes.update, this.addItemRouteParameters());
       }
-      return this.route(this.routes.store);
+      return this.route(this.routes.store, this.addItemRouteParameters());
     },
 
     isFormValid() {
@@ -70,28 +76,77 @@ export default {
         return;
       }
 
-      //  this.form.state.isSubmitting = true;
+      this.form.state.isSubmitting = true;
       this.clearResponses();
 
       const postData = this.preProcessData(this.form.data)
 
       const url = this.getSaveUrl()
       this.$http
-          .post(url, postData)
-          .then((success) => {
-            this.form.messages.success = success.data.message;
-            this.isDirty = false;
-            this.$emit('saved');
-            this.back();
-          })
-          .catch((error) => {
-            this.form.messages.error = error.response.data.message;
-            this.form.apiErrors = error.response.data.data;
-            this.resetLoadingState();
-          })
+        .post(url, postData)
+        .then((success) => {
+          this.form.messages.success = success.data.message;
+          this.isDirty = false;
+          this.$emit('saved');
+          this.back();
+        })
+        .catch((error) => {
+          console.log(error);
+          this.form.messages.error = error.response.data.message;
+          this.form.apiErrors = error.response.data.data;
+          this.resetLoadingState();
+        })
         .finally(() => {
         });
+    },
 
+    // refactor so that function use eithee the POST or PUT method
+    update() {
+      if (!this.isFormValid()) {
+        return;
+      }
+
+      this.form.state.isSubmitting = true;
+      this.clearResponses();
+
+      const postData = this.preProcessData(this.form.data)
+
+      const url = this.route(this.routes.update, this.addItemRouteParameters());
+
+      this.$http
+        .put(url, postData)
+        .then((success) => {
+          this.form.messages.success = success.data.message;
+          this.isDirty = false;
+          this.$emit('saved');
+          this.back();
+        })
+        .catch((error) => {
+          console.log(error);
+          this.form.messages.error = error.response.data.message;
+          this.form.apiErrors = error.response.data.data;
+          this.resetLoadingState();
+        })
+        .finally(() => {
+        });
+    },
+
+    deleteAgreedModel() {
+      const url = this.route(this.routes.destroy, this.addItemRouteParameters());
+
+      this.$http
+        .delete(url)
+        .then((success) => {
+          this.form.messages.success = success.data.message;
+          this.$inertia.get(this.redirectAfterDelete());
+        })
+        .catch((error) => {
+          this.form.apiErrors = error.response.data.data;
+          this.form.messages.error = error.response.data.message;
+          this.form.state.isSubmitting = false;
+        })
+        .finally(() => {
+        });
     },
 
     clearResponses() {
