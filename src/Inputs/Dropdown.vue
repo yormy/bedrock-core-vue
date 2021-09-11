3830475fa0fb32e333a01b29bc2b065f683223c4
rob <dev@mexion.com>
2021-09-11 17:33:08 +0200

    display api errors

--- src/Inputs/Dropdown.vue
+++ src/Inputs/Dropdown.vue
@@ -5,6 +5,8 @@
       v-on="$listeners"
       :items="items"
       :value="value"
+      :color="isValid"
+      :error-messages="apiError ? apiError : errors"
     >
     </v-select>
   </div>
@@ -18,14 +20,54 @@ export default {
       required: false,
     },
 
+    fieldname: {
+      type: String,
+      required: false,
+    },
+
     items: {
       type: Array,
       required: false,
+    },
+
+    apiErrors: {
+      type: Object,
+      required: false,
+    },
+  },
+
+  computed: {
+    isValid() {
+      if (
+        (!this.apiErrors || !this.apiErrors.errors || !this.apiErrors.errors[this.fieldname]) &&
+        this.value
+      ) {
+        return 'success';
+      }
+
+      return '';
+    },
+
+    apiError() {
+      if (!this.apiErrors || !this.apiErrors.errors) {
+        return '';
+      }
+      return this.apiErrors.errors[this.fieldname];
     },
   },
 
   data() {
     return {};
+  },
+
+
+  methods: {
+    clearApiError() {
+      if (this.apiErrors && this.apiErrors.errors) {
+        // eslint-disable-next-line vue/no-mutating-props
+        this.apiErrors.errors[this.fieldname] = '';
+      }
+    },
   },
 
 };
