faeaafef66e4e7bd7aaa48947029671d70c4d938
rob <dev@mexion.com>
2022-07-10 14:32:39 +0200

    add icon abstraction

---
+++ src/Components/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <span :class="icon"></span>
+</template>
+
+<script>
+
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
